1913eaafbd1dde1bf88b4ac9bc98335b17ac3610
Victoria Inahuazo <victoria.inahuazo11@myhunter.cuny.edu>
2024-03-12 11:55:24 -0400

    Add files via upload
    classwork for experimenting with csv files and generating tables. the code prints the luminosity of the star, temperature of the star and the average radius of a hypergiant

---
+++ stars1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+#store the CSV file as a DataFrame 
+stars = pd.read_csv("stars2.csv")
+
+#Prints the luminosity of the brightest star.
+print("Luminosity of brighest star:", stars['Luminosity(L/Lo)'].max())
+
+#Prints the temperature of the coldest star.
+print("Temperature of coldest star:", stars['Temperature(K)'].min())
+
+#Prints the average radius of a Hypergiant.
+groupedData = stars.groupby('Star type')
+hypergiant = groupedData.get_group('Hypergiant')
+
+print("Hypergiant average radius:", hypergiant['Radius(R/Ro)'].mean())
+
+#same as above but all on one line
+print(stars.groupby('Star type').get_group('Hypergiant')['Radius(R/Ro)'].mean())
